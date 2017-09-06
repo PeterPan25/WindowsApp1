cfb7c7a185c548eaccdec69a877d1c90a0373045
PeterPan25 <31684969+PeterPan25@users.noreply.github.com>
2017-09-06 14:20:34 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
+
+    End Sub
+
+    Private Sub LBname_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LB_Name.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub LL_Kinderarzt_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LL_Kinderarzt.LinkClicked
+
+    End Sub
+
+    Private Sub B_öffne_Click(sender As Object, e As EventArgs) Handles B_öffne.Click
+        Dim name As String
+        name = Me.LB_Name.Text
+        Form(name).show
+
+    End Sub
+End Class
